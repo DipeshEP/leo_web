fb83408e3ace3efd5b44023d6d2e335c9da8a4f4
DIpu <deepuep@gmail.com>
2022-09-13 10:15:55 +0530

    initial commit

--- lib/src/pages/appcolors/app_colors.dart
+++ lib/src/pages/appcolors/app_colors.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/cupertino.dart';
 
-class AppColor{
-  final white=const Color(0XFFFFFFFF);
-  final buttonWhite=const Color(0XFFE8E8E8);
-  final appBlue=const Color(0XFF055E98);
-  final appDarkBlue=const Color(0XFF075E94);// this is the app blue color
-  final loginWhite=const Color(0XFFECE9E6);// this is the white color , you can call like AppTheme.colors.loginwhite
-  final black=const Color(0XFF000000);
-  final blue=const Color(0XFF0075FF);
-  final signUpBlue=const Color(0XFF0075FF);
-  final loginBlur=const Color(0XFFA1A1A1);
-  final shadowWhite=const Color(0XFFF2F2F2);
-  final containerShadow=const Color(0XFF0A8EE1);
-  final logoColor=const Color(0XFFB9B9B9);
-  final green=const Color(0XFF06FE1F);
+class AppColor {
+  final white = const Color(0XFFFFFFFF);
+  final buttonWhite = const Color(0XFFE8E8E8);
+  final appBlue = const Color(0XFF055E98);
+  final appDarkBlue = const Color(0XFF075E94); // this is the app blue color
+  final loginWhite = const Color(
+      0XFFECE9E6); // this is the white color , you can call like AppTheme.colors.loginwhite
+  final black = const Color(0XFF000000);
+  final blue = const Color(0XFF0075FF);
+  final signUpBlue = const Color(0XFF0075FF);
+  final loginBlur = const Color(0XFFA1A1A1);
+  final shadowWhite = const Color(0XFFF2F2F2);
+  final containerShadow = const Color(0XFF0A8EE1);
+  final logoColor = const Color(0XFFB9B9B9);
+  final green = const Color(0XFF06FE1F);
   AppColor();
 }
